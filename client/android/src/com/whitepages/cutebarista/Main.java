9fe70c9221d74fd7b5493b1af8993cb994ae2cda
Sebastian Beresniewicz <sberesni@gmail.com>
2011-04-29 12:57:43 -0700

    add eclipse files, update class path, and change name of the main activity

--- client/android/src/com/whitepages/cutebarista/Main.java
+++ client/android/src/com/whitepages/cutebarista/Main.java
@@ -1,11 +1,9 @@
 package com.whitepages.cutebarista;
 
-import com.whitepages.cutebarista.R;
-
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainScreen extends Activity {
+public class Main extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
