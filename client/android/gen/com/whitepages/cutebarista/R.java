ddd135ee542a1c436b5d22668377169ee792eb3b
Sebastian Beresniewicz <sberesni@gmail.com>
2011-04-30 00:47:18 -0700

    Updated resources

--- client/android/gen/com/whitepages/cutebarista/R.java
+++ client/android/gen/com/whitepages/cutebarista/R.java
@@ -10,14 +10,36 @@ package com.whitepages.cutebarista;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background_color=0x7f040000;
+        public static final int top_text_color=0x7f040001;
+    }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int ca_cute_32=0x7f020000;
+        public static final int ca_cute_48=0x7f020001;
+        public static final int ca_find_32=0x7f020002;
+        public static final int ca_find_48=0x7f020003;
+        public static final int ca_logo_32=0x7f020004;
+        public static final int ca_logo_48=0x7f020005;
+        public static final int ca_not_32=0x7f020006;
+        public static final int ca_not_48=0x7f020007;
+        public static final int icon=0x7f020008;
+    }
+    public static final class id {
+        public static final int TextView01=0x7f060000;
+        public static final int bottom_text=0x7f060004;
+        public static final int button_cute=0x7f060002;
+        public static final int button_find_cute=0x7f060005;
+        public static final int button_not_cute=0x7f060003;
+        public static final int linearLayout1=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int bottom_title=0x7f050003;
+        public static final int hello=0x7f050000;
+        public static final int top_title=0x7f050002;
     }
 }
--- client/android/src/com/whitepages/cutebarista/Main.java
+++ client/android/src/com/whitepages/cutebarista/Main.java
@@ -10,4 +10,6 @@ public class Main extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    
 }
